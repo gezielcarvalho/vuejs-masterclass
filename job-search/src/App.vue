c80b81a2aaa23fa08d33ebee9cd2f5f14e44b38b
Geziel Carvalho <geziel.natal@gmail.com>
2024-09-17 19:31:58 -0400

    Refactor welcome component and main.ts imports, update code formatting and linting configurations, and remove HelloWorld component and its test

--- job-search/src/App.vue
+++ job-search/src/App.vue
@@ -1,22 +1,16 @@
 <script setup lang="ts">
   import { RouterLink, RouterView } from "vue-router";
-  import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
-
   <RouterView />
 </template>
 
@@ -24,11 +18,6 @@
   header {
     line-height: 1.5;
     max-height: 100vh;
-  }
-
-  .logo {
-    display: block;
-    margin: 0 auto 2rem;
   }
 
   nav {
@@ -69,6 +58,7 @@
 
     header .wrapper {
       display: flex;
+      flex-direction: column;
       place-items: flex-start;
       flex-wrap: wrap;
     }
