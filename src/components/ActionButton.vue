4c52abcfd77f72cf34819e01a2a93b0a5f8d04c6
Geziel Carvalho <geziel.natal@gmail.com>
2024-10-25 20:51:44 -0400

    Refactor ActionButton component to use props for title

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,7 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  // declare a prop named `title` of type string, that will be received from the parent component
+  import { defineProps } from "vue";
+  interface IActionButtonProps {
+    title: string;
+  }
+  const props: IActionButtonProps = defineProps({
+    title: {
+      type: String,
+      required: true
+    }
+  });
+</script>
 <template>
   <button
     class="rounded border-0 bg-brand-blue-1 px-5 py-2 font-medium text-white hover:shadow-blue">
-    Sign In
+    {{ props.title }}
   </button>
 </template>
--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -53,7 +53,7 @@ components (MainNav, views like TeamsView).
         </ul>
       </div>
       <ProfileImage v-if="isLoggedIn" @click="toggleLogin" />
-      <ActionButton v-else @click="toggleLogin" />
+      <ActionButton v-else :title="'Sign In'" @click="toggleLogin" />
     </div>
   </nav>
   <p class="p-16">
