332bd6c71d0b08e74ffd5d696cc9cebfe8ae9d97
Geziel Carvalho <geziel.natal@gmail.com>
2024-10-27 11:28:14 -0400

    Refactor ActionButton component to use apply tailwind directive

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
   // declare a prop named `title` of type string, that will be received from the parent component
   import { defineProps } from "vue";
+
   interface IActionButtonProps {
     title: string;
   }
+
   const props: IActionButtonProps = defineProps({
     title: {
       type: String,
@@ -12,8 +14,22 @@
   });
 </script>
 <template>
-  <button
-    class="rounded border-0 bg-brand-blue-1 px-5 py-2 font-medium text-white hover:shadow-blue">
+  <button class="primary">
     {{ props.title }}
   </button>
 </template>
+
+<style scoped>
+  button {
+    @apply rounded px-5 py-2 font-medium;
+  }
+
+  .primary {
+    @apply border-0 bg-brand-blue-1 text-white hover:shadow-blue;
+  }
+
+  .secondary {
+    background-color: #f3f4f6;
+    color: #1e40af;
+  }
+</style>
